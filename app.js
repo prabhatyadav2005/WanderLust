ba472845b48928ab7805520bf9f59ba848764e10
prabhatyadav2005 <prabhatyadav4907@gmail.com>
2026-08-11 08:23:32 +0530

    validation_for_reviews

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js");
+const {listingSchema , reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
 
@@ -43,6 +43,17 @@ app.use(express.static(path.join(__dirname, "public")));
 // });
 
 const validateListing = (req, res, next) =>{
+    let {error} = listingSchema.validate(req.body);
+
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};
+
+const validateReview = (req, res, next) =>{
     let {error} = listingSchema.validate(req.body);
 
     if(error){
@@ -146,7 +157,7 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 //Reviews 
 //Post Route
 
-app.post("/listings/:id/reviews", async (req, res) => {
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
    let listing = await Listing.findById(req.params.id);  
    let newReview = new Review(req.body.review);           
 
@@ -156,7 +167,7 @@ app.post("/listings/:id/reviews", async (req, res) => {
    await listing.save();
 
    res.redirect(`/listings/${listing._id}`);
-});
+}));
 
 
 

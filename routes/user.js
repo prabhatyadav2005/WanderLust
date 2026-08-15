29d9a6e6235bbb391b1ff79c22b60b0b1656a239
prabhatyadav2005 <prabhatyadav4907@gmail.com>
2026-08-15 23:01:49 +0530

    Automatic login after signUp

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User  = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport  = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
 
 
 router.get("/signup", (req, res) =>{
@@ -17,13 +18,19 @@ router.post("/signup",
             const newUser = new User({email, username});
             const registeredUser = await User.register(newUser, password);
             console.log(registeredUser);
-            req.flash("success","Welcome to Wandeerlust! Developed By PRABHAT YADAV" );
-            res.redirect("/listings");
+
+            req.login(registeredUser, (err) =>{
+                if(err){
+                    return next(err);
+                }
+                req.flash("success","Welcome to Wandeerlust! Developed By PRABHAT YADAV" );
+                res.redirect("/listings");
+            });
         }catch(e){
             req.flash("error",e.message);
             res.redirect("/signup");
 
-        }
+    }
 
 }));
 
@@ -31,14 +38,16 @@ router.get("/login", (req, res) =>{
     res.render("users/login.ejs");
 });
 
-router.post("/login", 
+router.post(
+    "/login", 
+    saveRedirectUrl,
     passport.authenticate("local", {
         failureRedirect: '/login', 
         failureFlash : true
     }), 
     async(req, res) =>{
         req.flash("success","Welcome to Wanderlust! You are logged in!");
-        res.redirect("/listings");
+        res.redirect(res.locals.redirectUrl || "Listings");
 });
 
 

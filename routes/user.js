449b6ff4d5aa3b27f41e9eacffe35e7ee41d94c7
prabhatyadav2005 <prabhatyadav4907@gmail.com>
2026-08-15 17:03:28 +0530

    Login User

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User  = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport  = require("passport");
+
 
 router.get("/signup", (req, res) =>{
     res.render("users/signup.ejs");
@@ -22,6 +24,21 @@ router.post("/signup",
             res.redirect("/signup");
 
         }
-    
+
 }));
+
+router.get("/login", (req, res) =>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login", 
+    passport.authenticate("local", {
+        failureRedirect: '/login', 
+        failureFlash : true
+    }), 
+    async(req, res) =>{
+        req.flash("success","Welcome to Wanderlust! You are logged in!");
+        res.redirect("/listings");
+});
+
 module.exports = router;

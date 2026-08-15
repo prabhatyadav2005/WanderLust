4c71a44323223d1293f4ccd3f75cea15df0f70d5
prabhatyadav2005 <prabhatyadav4907@gmail.com>
2026-08-15 16:29:32 +0530

    SignUp User

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ const User = require("./models/user.js");
 const {listingSchema, reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 
 
@@ -87,8 +88,9 @@ app.get("/demouser", async (req, res) =>{
    res.send(registeredUser);
 });
 
-app.use("/listings" , listings);
-app.use("/listings/:id/reviews" , reviews);
+app.use("/listings" , listingRouter);
+app.use("/listings/:id/reviews" , reviewRouter);
+app.use("/", userRouter);
 
 
 app.all("*splat", (req, res, next) =>{

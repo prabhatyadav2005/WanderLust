bc158f4c092e6f485671c43475ba0d2af2e8f3c4
prabhatyadav2005 <prabhatyadav4907@gmail.com>
2026-08-16 16:53:50 +0530

    Authorization for Listings

--- routes/listing.js
+++ routes/listing.js
@@ -1,21 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const {listingSchema , reviewSchema} = require("../schema.js");
+const { reviewSchema} = require("../schema.js");
 const Listing = require("../models/listing.js");
-const{isLoggedIn} = require("../middleware.js");
+const{isLoggedIn, isOwner,validateListing } = require("../middleware.js");
 
-const validateListing = (req, res, next) =>{
-    let {error} = listingSchema.validate(req.body);
-
-    if(error){
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-};
 
 //index route:
 router.get("/", wrapAsync(async (req, res) =>{
@@ -49,6 +38,7 @@ router.get("/:id", wrapAsync(async (req,res) =>{
 router.post(
     "/",
     isLoggedIn,
+    
     validateListing,
     wrapAsync (async( req, res, next) => {
     
@@ -62,7 +52,10 @@ router.post(
 
 
 //Edit Route
-router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
+router.get("/:id/edit", 
+    isLoggedIn,
+    isOwner,
+    wrapAsync(async (req, res) => {
     
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -76,6 +69,7 @@ router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
 //Update Route
 router.put("/:id",
     isLoggedIn,
+    isOwner,
     validateListing,
     wrapAsync(async (req, res) => {
     
@@ -104,6 +98,7 @@ router.put("/:id",
 router.delete(
     "/:id", 
     isLoggedIn,
+    isOwner,
     wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);

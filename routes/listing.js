3b8e9a3c455ff877dbbd2b01ef567a403a35726c
prabhatyadav2005 <prabhatyadav4907@gmail.com>
2026-08-18 20:00:31 +0530

    Cloud Setup & Store file

--- routes/listing.js
+++ routes/listing.js
@@ -4,11 +4,11 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { reviewSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
 
 const multer = require('multer');
-const upload  = multer({dest : 'uploads/'});
+const { storage } = require("../cloudConfig.js");
+const upload = multer({storage});
 
 router
      .route("/")

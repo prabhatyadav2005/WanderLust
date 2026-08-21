1f7be8a399afa3501b8cc3f162f6e1f4b51bcd11
prabhatyadav2005 <prabhatyadav4907@gmail.com>
2026-08-21 23:11:25 +0530

    Geocoding, (converting locations into coordinates

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose  = require("mongoose");
 const Schema = mongoose.Schema;
 const Review  = require("./review.js");
+const { required } = require("joi");
 
 const listingSchema = new Schema({
     title : {
@@ -23,6 +24,17 @@ const listingSchema = new Schema({
  owner : {
     type: Schema.Types.ObjectId,
     ref: "User",
+ },
+ geometry: {
+    type: {
+    type: String, //Don't do ` { location: { type: String}}`
+    enum: ['Point'],
+    required: true
+ },
+ coordinates : {
+    type: [Number],
+    required: true
+  },
  },
 });
 
